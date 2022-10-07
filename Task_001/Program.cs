d84060f2811348230ea1e8458776bd0970e68dd8
Egor <egs046@gmail.com>
2022-10-07 14:55:21 +0300

    added tasks

---
+++ Task_001/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+int EnterLength()
+{
+    Console.WriteLine("Введите длину массива: ");
+    int length = int.Parse(Console.ReadLine() ?? "");
+    return length;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length-1]);
+}
+
+int[] GetArray(int number)
+{
+    int[] array = new int[number];
+    Console.WriteLine($"Введите значенение {number} элементов:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine() ?? "");
+    }
+    Console.WriteLine();
+    PrintArray(array);
+    return array;
+}
+
+int CheckElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine("Кол-во элементов: " + count);
+    return count;
+}
+
+int number = EnterLength();
+int[] array = GetArray(number);
+Console.WriteLine();
+int count = CheckElements(array);
---
+++ Task_002/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double GetNumber(string message)
+{
+    Console.WriteLine(message);
+    double number = double.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+double FindCrossX(double a, double c, double b, double d)
+{
+    double x = (d - c) / (a - b);
+    return x;
+}
+
+double FindCrossY (double b, double d, double x)
+{
+    double y = b*x + d;
+    return y;
+}
+
+
+double firstB = GetNumber("Введите координату пересечения первой прямой с осью: ");
+double firstK = GetNumber("Введите угловой коэфицент первой прямой: ");
+
+double secondB = GetNumber("Введите координату пересечения второй прямой с осью: ");
+double secondK = GetNumber("Введите угловой коэфицент второй прямой: ");
+
+
+if ((firstK==secondK) && (firstB != secondB))
+    Console.WriteLine("Прямые параллельны");
+else if ((firstK==secondK) && (firstB == secondB))
+    Console.WriteLine("Введена одна прямая");
+else
+{
+    double x = FindCrossX(firstK, firstB, secondK, secondB);
+    double y = FindCrossY(firstK, firstB, x);
+    Console.WriteLine($"Точка пересечения: ({x},{y})");
+}
